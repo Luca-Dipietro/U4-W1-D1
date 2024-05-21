c7c355002bb26d680636cdd3b369e0684bd835af
Luca-Dipietro <luca.dipietro.ld@gmail.com>
2024-05-21 09:47:41 +0200

    Fix es 3

--- src/Es3.java
+++ src/Es3.java
@@ -5,27 +5,26 @@ public class Es3 {
         Scanner sc = new Scanner(System.in);
 
         System.out.print("Inserisci il primo lato del rettangolo: ");
-        double lato1Rettangolo = sc.nextDouble();
+        double lato1Rettangolo = Double.parseDouble(sc.nextLine());
         System.out.print("Inserisci il secondo lato del rettangolo: ");
-        double lato2Rettangolo = sc.nextDouble();
+        double lato2Rettangolo = Double.parseDouble(sc.nextLine());
         double perimetroRett = perimetroRettangolo(lato1Rettangolo, lato2Rettangolo);
         System.out.println("Il perimetro del rettangolo è: " + perimetroRett);
 
         System.out.print("Inserisci un numero: ");
-        int numero = sc.nextInt();
-        int result = pariDispari(numero);
-        if (result == 0) {
-            System.out.println("Il numero è pari");
+        int numero = Integer.parseInt(sc.nextLine());
+        if (pariDispari(numero) == 0) {
+            System.out.println("Il numero " + numero + " è pari");
         } else {
-            System.out.println("Il numero è dispari");
+            System.out.println("Il numero " + numero + " è dispari");
         }
 
         System.out.print("Inserisci il primo lato del triangolo: ");
-        double lato1Triangolo = sc.nextDouble();
+        double lato1Triangolo = Double.parseDouble(sc.nextLine());
         System.out.print("Inserisci il secondo lato del triangolo: ");
-        double lato2Triangolo = sc.nextDouble();
+        double lato2Triangolo = Double.parseDouble(sc.nextLine());
         System.out.print("Inserisci il terzo lato del triangolo: ");
-        double lato3Triangolo = sc.nextDouble();
+        double lato3Triangolo = Double.parseDouble(sc.nextLine());
         double areaTriangolo = perimetroTriangolo(lato1Triangolo, lato2Triangolo, lato3Triangolo);
         System.out.println("L'area del triangolo è: " + areaTriangolo);
 
@@ -37,7 +36,11 @@ public class Es3 {
     }
 
     public static int pariDispari(int numero) {
-        return numero % 2 == 0 ? 0 : 1;
+        if (numero % 2 == 0){
+            return 0;
+        }else {
+            return 1;
+        }
     }
 
     public static double perimetroTriangolo(double lato1, double lato2, double lato3) {
